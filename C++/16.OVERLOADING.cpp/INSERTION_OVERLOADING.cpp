66b98119673e27c2307d773b05876ddb8bbc74c3
Ansuman <ansumanpanigrahi321@gmail.com>
2022-01-25 18:15:08 +0530

    check\

--- C++/16.OVERLOADING.cpp/INSERTION_OVERLOADING.cpp
+++ C++/16.OVERLOADING.cpp/INSERTION_OVERLOADING.cpp
@@ -6,19 +6,37 @@ using namespace std;
 //overloading of insertion and extraction operators using friend funtion
 class complex
 {
-    int real;
-    int img;
-    public:
-    complex(int real=0,int img=0)
-    {
-        this->real=real;
-        this->img=img;
-    }
-    
+	public:
+	int real;
+	int img;
+public:
+	complex(int real = 0, int img = 0)
+	{
+		this->real = real;
+		this->img = img;
+	}
+	void display()
+	{
+		cout<<real<<" + i"<<img<<endl;
+	}
+	friend ostream& operator<<(ostream& o, complex& c1);
+
 };
+ostream &operator<<(ostream& o, complex& c1)
+{
+	o << c1.real << " + i" << c1.img << endl;
+	
+
+}
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int a, b;
+	cin >> a >> b;
+	complex test(a, b);
+	complex test1;
+	cout << test << endl;
+	test.display();
+	return 0;
 }
--- C/11.switchcase.c
+++ C/11.switchcase.c
@@ -8,19 +8,19 @@ int main()
     switch (rating)
     {
     case 1:
-        printf("your rating has been registerd (1) \n");
+        printf("your rating has been registered (1) \n");
         break;
     case 2:
-        printf("your rating has been registerd (2) \n");
+        printf("your rating has been registered (2) \n");
         break;
     case 3:
-        printf("your rating has been registerd (3) \n");
+        printf("your rating has been registered (3) \n");
         break;
     case 4:
-        printf("your rating has been registerd (4) \n");
+        printf("your rating has been registered (4) \n");
         break;
     case 5:
-        printf("your rating has been registerd (5) \n");
+        printf("your rating has been registered (5) \n");
         break;
     default:
         printf("please enter a valid rating");
