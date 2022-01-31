fdb36fc6c695fc072452664b24dbab24697a1f08
Ansuman <ansumanpanigrahi321@gmail.com>
2022-01-31 19:30:29 +0530

    ccc

---
+++ C++/20.FRIEND AND STATIC MEMBERS/STATIC_MEMBERS.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <math.h>
+using namespace std;
+class base
+{
+public:
+    static int count; // static variable is allocated only once
+    static int fun()
+    {
+
+        count++;
+    }
+    static int getcount()
+    {
+        return count; // static functions can only access static variables
+    }
+};
+int base::count = 0; // this means the static variable can only be used inside the base class
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    base b;
+    b.fun();
+    // or we can call it by scope resoluton operator if the function is static
+    base::fun();
+    return 0;
+}
