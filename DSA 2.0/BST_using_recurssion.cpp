0668b62e467ce2413252f7d22d9b330409cfa4d8
Ansuman <ansumanpanigrahi321@gmail.com>
2022-04-04 19:35:27 +0530

    check

---
+++ DSA 2.0/BST_using_recurssion.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int BinarySearch(int arr[], int num, int start, int end)
+{
+    int mid;
+
+    if (start > end)
+    {
+
+        cout << "number not present";
+        return 0;
+    }
+    else
+    {
+
+        mid = (start + end) / 2;
+
+        if (arr[mid] == num)
+        {
+
+            cout << "address :: " << mid << " index \n";
+            return 0;
+        }
+        else if (num > arr[mid])
+        {
+
+            BinarySearch(arr, num, mid + 1, end);
+        }
+        else if (num < arr[mid])
+        {
+
+            BinarySearch(arr, num, start, mid - 1);
+        }
+    }
+}
+
+int main()
+{
+
+    int arr[100], key, i, size, start, end;
+
+    cout << "Enter the size \n";
+    cin >> size;
+
+    cout << "Enter  values \n";
+
+    for (i = 0; i < size; i++)
+    {
+
+        cin >> arr[i];
+    }
+    sort(arr[0], arr[0] + size);
+    cout << "Enter a value to be search \n";
+    cin >> key;
+
+    start = 0;
+    end = size - 1;
+
+    BinarySearch(arr, key, start, end);
+
+    return 0;
+}
---
+++ DSA 2.0/binary_search.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int lower;
+    int higher;
+    int middle;
+    middle = lower + higher;
+    middle /= 2;
+    return 0;
+}
