2f39012681b43c619d6bad017f96453fec72f6d0
Ansuman <ansumanpanigrahi321@gmail.com>
2022-04-04 19:35:32 +0530

    check 1

--- DSA 2.0/Array_func.cpp
+++ DSA 2.0/Array_func.cpp
@@ -1,10 +1,32 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+int insert(int address, int value, int *arr, int size)
+{
+
+    for (int i = size; i > address; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[address] = value;
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    // array insert function
+    int size;
+    cin >> size;
+    int arr[size + 1];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> *(arr + i);
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << *(arr + i) << " ";
+    }
+
+    insert(5, 10, &arr[0], size);
     return 0;
 }
