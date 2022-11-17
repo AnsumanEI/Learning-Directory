2223c3f4b4af8b2e8dab0c468576cc0759fee822
Ansuman <ansumanpanigrahi321@gmail.com>
2022-11-17 19:01:11 +0530

    Co-authored-by: Rudrax02 <Rudrax02@users.noreply.github.com>

---
+++ DSA-FULL/RECURSION/MULTIPLE RECURSION/multiplerecursion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <math.h>
+#include <strings.h>
+#include <string>
+#include <cstring>
+using namespace std;
+int fib(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << fib(i) << " ";
+    }
+    return 0;
+}
---
+++ DSA-FULL/RECURSION/subsequence.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <math.h>
+#include <strings.h>
+#include <string>
+#include <cstring>
+using namespace std;
+int main()
+{
+    // sub array but with sequence maintained is called sub seqence
+    return 0;
+}
