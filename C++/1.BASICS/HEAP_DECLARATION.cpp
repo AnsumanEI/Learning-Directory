a79c1bc595cfc68a9f447de98c5ee861348ce645
Ansuman <ansumanpanigrahi321@gmail.com>
2023-08-21 20:00:37 +0530

    2023check

--- C++/1.BASICS/HEAP_DECLARATION.cpp
+++ C++/1.BASICS/HEAP_DECLARATION.cpp
@@ -5,6 +5,9 @@ int main()
 
     int *p = new int[5];
     *(p + 1) = 2;
+
+    cout<<&p<<" "<<&*(p+1)<<" "<<&p[1];//2nd and third are the same way of diffrent things
+    
     for (int i = 0; i < 5; i++)
     {
         cout << *(p + i) << endl;
