6f64a5dd17b8c08def95fd987e93dae962e06636
Ansuman <ansumanpanigrahi321@gmail.com>
2023-08-24 22:10:16 +0530

    check

--- check.cpp
+++ check.cpp
@@ -8,41 +8,56 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> twoSum(vector<int> &nums, int target)
+    vector<int> searchRange(vector<int> &nums, int target)
     {
+        int l = 0;
+        int r = nums.size() - 1;
 
-                int l = 0, r = nums.size() - 1;
-        vector<int> ans = {};
-        int mid = l + (r - l) / 2;
-        while (l <= r && l + r != 0)
+        vector<int> ans(2, -1);
+        while (l <= r && r >= 0 && l >= 00)
         {
-            mid = l + (r - l) / 2;
-            if (nums[mid] <= target)
+            int mid = l + (r - l) / 2;
+            if (mid < 0 || mid > nums.size() - 1 || r < 0)
             {
-                target -= nums[mid];
-                r = mid;
-                ans.push_back(mid);
+                break;
             }
-            else
+            if (nums[mid] == target)
+            {
+
+                if (nums[mid - 1] == target)
+                {
+                    ans[0] = mid - 1;
+                }
+                else if (nums[mid + 1] == target)
+                {
+                    ans[0] = mid + 1;
+                }
+                else
+                {
+                    ans[0] = mid;
+                }
+                ans[1] = mid;
+
+                break;
+            }
+            else if (nums[mid] < target)
             {
                 l = mid + 1;
             }
+            else
+            {
+                r = mid - 1;
+            }
         }
-        if (target == 0)
-        {
-            return ans;
-        }
-        else
-        {
-            return {};
-        }
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
 int main()
 {
     Solution a;
-    vector<int> grid = {3, 2, 4};
-    int target = 9;
-    vector<int> res = a.twoSum(grid, target);
-    cout << res.at(1) << " " << res.at(0);
+    vector<int> grid = {1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7};
+    int target = 2;
+    vector<int> ans1 = a.searchRange(grid, target);
+    cout << ans1.at(0) << ans1.at(1) << endl;
 }
