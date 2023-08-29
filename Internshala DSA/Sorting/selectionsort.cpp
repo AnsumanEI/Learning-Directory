cce9297d9501280a4f4d7d9ca4cdba6bbaba6976
AnsumanEI <ansumanpanigrahi321@gmail.com>
2023-08-29 22:38:24 +0530

    sort

--- Internshala DSA/Sorting/selectionsort.cpp
+++ Internshala DSA/Sorting/selectionsort.cpp
@@ -7,22 +7,18 @@
 using namespace std;
 vector<int> bubblesort(vector<int> &arr)
 {
-    int count = 0;
     int size = arr.size();
     for (int i = 0; i < size - 1; ++i) // no. of passes
     {
-        for (int j = 0; j < (size - i) - 1; ++j)
+        for (int j = i + 1; j < size; ++j)
         {
-            if (arr[j] > arr[j + 1])
+            if (arr[i] > arr[j])
             {
-                count++;
                 int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
         }
-        if (count == 1) // so that if already sorted then no passes are used
-            return arr;
     }
     return arr;
 }
@@ -36,13 +32,9 @@ void display(vector<int> &arr)
 int main()
 {
     // logic
-    // passes means the stages of sorting
-    // adjacent elements are compared and swapped
-    // eg 5 elements are there
-    // 1st pass 1 to n-1 = 4 passes the largest will settle on the last
-    // 2nd pass 1 to n-2 = 3 passes the 2nd largest will settle on the 2nd last
-    // so on and so forth
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // same as bubble sort but the the number which is compared is not switched
+
+    vector<int> arr;
     srand(time(0));
     for (int i = 0; i < 10; i++)
     {
--- Internshala DSA/search/binary.cpp
+++ Internshala DSA/search/binary.cpp
@@ -5,8 +5,34 @@
 #include <string>
 #include <cstring>
 using namespace std;
+int binarysearch(vector<int> &arr, int target)
+{
+    int l = 0;
+    int r = arr.size() - 1;
+
+    int mid = l + (r - l) / 2;
+    while (l <= r)
+    {
+        mid = l + (r - l) / 2;
+        if (arr[mid] < target)
+        {
+            l = mid + 1;
+        }
+        else
+        {
+            r = mid - 1;
+        }
+    }
+    return l + 1;
+}
+
 int main()
 {
 
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    sort(arr.begin(), arr.end());
+    int target = 6;
+    int ans = binarysearch(arr, target);
+    cout << ans;
     return 0;
 }
