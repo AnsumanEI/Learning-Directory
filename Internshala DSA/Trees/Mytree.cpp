ecaf2f34a20b06fc0766e432b6be8f7b87c6a422
AnsumanEI <ansumanpanigrahi321@gmail.com>
2023-09-26 01:01:20 +0530

    mytree

--- Internshala DSA/Trees/Mytree.cpp
+++ Internshala DSA/Trees/Mytree.cpp
@@ -5,8 +5,76 @@
 #include <string>
 #include <cstring>
 using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+node *buildtree(node *&root)
+{
+    int data;
+    cout << "enter data :" << endl;
+    cin >> data;
+
+    root = new node(data);
+    if (data == -1)
+    {
+        return NULL;
+    }
+    cout << "enter the value for left of " << data << ":\n";
+    root->left = buildtree(root->left);
+
+    cout << "enter the value for right of " << data << ":\n";
+    root->right = buildtree(root->right);
+    return root;
+}
+
+void levelordertrav(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+} // 1 2 3 -1 -1 4 -1 -1 5 6 -1 8 -1 -1 7 9 -1 -1 -1
 int main()
 {
-
+    node *root = NULL;
+    buildtree(root);
+    cout << "print \n";
+    levelordertrav(root);
     return 0;
 }
