c3f7071475fd112e872828c54a5dcfebb97edaf5
AnsumanEI <ansumanpanigrahi321@gmail.com>
2026-03-22 11:56:13 +0530

    refactor: complete folder reorganization into professional structure

--- 01_languages/java/arrays.java
+++ 01_languages/java/arrays.java
@@ -1,24 +1,16 @@
 
+import java.util.Arrays;
+
 public class arrays {
-    public static void add(int[] arr)// its like call by value in c++
-    {
-        System.out.println("value inside arr-" + arr[0]);
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
-        arr[3] = 4;
-    }
-
-    public static void print(int arr[]) {
-        for (int val : arr) {
-            System.out.println(val);
-        }
-    }
-
-    // there is a function for arr length called arr.length
     public static void main(String[] args) {
-        int arr[] = new int[10];
-        add(arr);
-        print(arr);
+        int[] userArray = { 22, 11, 2, 34, 48 };
+        String[] useStrings = { "hi", "bye", "seeyou" };
+        Arrays.sort(userArray);
+        // Arrays.fill(userArray, 0);
+        for (int val : userArray) {
+            System.out.println(val);
+            System.out.println(useStrings[0]);
+        }
+
     }
 }
